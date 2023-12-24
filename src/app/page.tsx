8f58fb74fae34f353ded08a68629a55c506e58a6
MaratFa <evolution99@rambler.ru>
2023-12-24 20:08:03 +0300

    Edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return <p>Hello world</p>;
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Your marketplace for high-qality{" "}
+            <span className="text-blue-600">digital assets</span>.
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcome to Digital Marketplace. Every asset on our platform is
+            verified by our team to ensure our highest qulity standards.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href="/products" className={buttonVariants()}>
+              Browse Thending
+            </Link>
+            <Button variant="ghost">Our quality promisef &rarr;</Button>
+          </div>
+        </div>
+
+        {/* TODO: List products */}
+      </MaxWidthWrapper>
+
+      <section></section>
+    </>
+  );
 }
