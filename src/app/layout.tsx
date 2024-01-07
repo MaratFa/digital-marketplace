356a1276b9c6c6dfdd6b5e1a6c34276291e054d3
MaratFa <evolution99@rambler.ru>
2024-01-07 05:31:51 +0300

    Edited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className="relative flex flex-col min-h-screen">
+          <Providers></Providers>
           <Navbar />
           <div className="flex-grow flex-1">{children}</div>
         </main>
